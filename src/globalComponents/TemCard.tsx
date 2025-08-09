d406bf28ba06990de14445680ce534570fdac2a0
md jakria ahmod <jakariaahmodmd@gmail.com>
2025-08-09 19:15:44 +0600

    temecard resposive done

--- src/globalComponents/TemCard.tsx
+++ src/globalComponents/TemCard.tsx
@@ -13,17 +13,17 @@ const TemCard: React.FC<CardDetails> = ({
 }) => {
   return (
     <section>
-      <div className="p-[24px] group shadow transition-all hover:bg-primary cursor-pointer  text-white rounded-[16px]">
-        <h4 className="text-[#666] text-[72px] group-hover:text-white group-hover:opacity-100 opacity-20 font-bold leading-[96px]">
+      <div className="p-6 group shadow transition-all hover:bg-primary cursor-pointer text-white rounded-[16px]">
+        <h4 className="text-[#666] text-[72px] sm:text-[56px] xs:text-[40px] group-hover:text-white group-hover:opacity-100 opacity-20 font-bold leading-[96px] sm:leading-[72px] xs:leading-[52px]">
           {number}
         </h4>
         <div>
-          <strong className="text-primary group-hover:text-white font-Roboto text-[24px] font-bold leading-[31px]">
+          <strong className="text-primary group-hover:text-white font-Roboto text-[24px] sm:text-[20px] xs:text-[18px] font-bold leading-[31px]">
             {title}
           </strong>
         </div>
-        <div className="mt-[16px]">
-          <p className="text-[#666] group-hover:text-white font-Roboto text-base font-normal leading-[24px]">
+        <div className="mt-4">
+          <p className="text-[#666] group-hover:text-white font-Roboto text-base sm:text-sm xs:text-sm font-normal leading-[24px]">
             {description.substring(0, 230)}.....
           </p>
         </div>
