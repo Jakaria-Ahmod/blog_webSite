2a096b197b0fd998ee183a834e884e90585d51d1
md jakria ahmod <jakariaahmodmd@gmail.com>
2025-08-09 18:17:41 +0600

    responsive work start

--- src/comonLayout/Heder/index.tsx
+++ src/comonLayout/Heder/index.tsx
@@ -1,18 +1,38 @@
+import { Menu as MenuIcon, X } from 'lucide-react'; // hamburger icon
+import React from 'react';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
 
 const Heder: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
-    <div>
-      <div className="container mx-auto flex  justify-between sticky top-0 left-0 w-full py-[21px]">
-        <div>
-          <Logo></Logo>
-        </div>
-        <div>
-          <Menu></Menu>
-        </div>
+    <header className="sticky top-0 left-0 w-full bg-white shadow-sm z-50">
+      <div className="container mx-auto flex items-center justify-between py-[21px] px-4 md:px-0">
+        {/* Logo */}
+        <Logo />
+
+        {/* Desktop Menu */}
+        <nav className="hidden md:block">
+          <Menu />
+        </nav>
+
+        {/* Mobile Menu Button */}
+        <button
+          className="md:hidden text-gray-800 focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X size={28} /> : <MenuIcon size={28} />}
+        </button>
       </div>
-    </div>
+
+      {/* Mobile Menu Dropdown */}
+      {isOpen && (
+        <div className="md:hidden bg-white shadow-inner">
+          <Menu />
+        </div>
+      )}
+    </header>
   );
 };
 
